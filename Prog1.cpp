e8f751f19f3e74fa3c403586bafac300e0b208f2
EzeP99 <ezep038@gmail.com>
2020-05-12 18:22:40 -0500

    commit program

--- Prog1.cpp
+++ Prog1.cpp
@@ -3,18 +3,53 @@
 #include <stdlib.h>
 using namespace std;
 
+int SecuencialDes();
+
 int main(){
 	SecuencialDes();
-	SecuencialOrd();
-	Bin();
+
 }
 
-void SecuencialDes(){
+int SecuencialDes(){
+	int n=0;
+	int x;
+	int i=0;
 	
-}
+	 	 srand(time(NULL)); //semilla numeros aleatorios
 
-void SecuencialOrd(){
+	 cout<<"Ingresa el tamaño del vector: "<<endl; //pide al usuario el tamaño del vector
+	 cin>>n;
+	 
+	 int v[n]; //declaramos vec ya que tenemos nuestro tamaño n.
+	 
+	 cout<<"Colocando valores aleatorios dentro del vector"<<endl;
+	 
+	 for(i=0; i<=n-1; i++){  //Llenamos el vector 
+	 
+	 	v[i] = 1+rand()%(n-0);
+	 }
+	 
+	 for (i=0; i<n;i++){
+	 	
+	 	cout<<"EL valor de i se le asigno"<<i<<"~"<<v[i];
+	 	cout<<endl;
+	 }
+	  while ((i<=n) && v[i]!= 0){
+    	i=i+1;
 }
+     if(i > n){
+	     cout<<"La informacion no esta en el arreglo";
+	     cout<<endl;
+ }else{
+	      cout<<"La informacion se encuentra en la posicion: "<<i<<endl;
+}
+	 
+	 return 0;
+	 system("PAUSE");
+	 }
+	
 
-void Bin(){
-}
+
+
+
+
