5a419fdcdfda86ce4a535eef227d11cbdfcfd60e
EzeP99 <ezep038@gmail.com>
2020-05-12 15:28:15 -0500

    primer commit

---
+++ Prog1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+using namespace std;
+
+int main(){
+	SecuencialDes();
+	SecuencialOrd();
+	Bin();
+}
+
+void SecuencialDes(){
+	
+}
+
+void SecuencialOrd(){
+}
+
+void Bin(){
+}
